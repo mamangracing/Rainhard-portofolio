50a2ba2549871294042430de70195d510bc6e681
mamangracing <putrabina0@gmail.com>
2021-02-17 18:54:05 +0700

    done

---
+++ asset/js/javascript.js
@@ -0,0 +1,15 @@
+// $(window).scroll(function(){
+// 	if($(this).scrollTop() > 100){
+// 		$('.slider').addClass('close');
+// 	} else {
+// 		$('.slider').removeClass('close');
+// 	} 
+
+// 	if($(this).scrollTop() > 150){
+// 		$('.slider2').addClass('close');
+// 	} else {
+// 		$('.slider2').removeClass('close');
+// 	}
+// });
+
+$('#fullpage').fullpage();
